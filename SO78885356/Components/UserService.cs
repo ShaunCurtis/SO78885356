04e721e53cdf5edd63409fc0305f90494a6b9506
Shaun Curtis <shaun@coldelm.com>
2024-08-20 16:32:16 +0100

    Add project files.

---
+++ SO78885356/Components/UserService.cs
@@ -0,0 +1,14 @@
+﻿namespace SO78885356.Components;
+
+public class UserService
+{
+    public string? AvatarName { get; private set; }
+
+    public event EventHandler<EventArgs>? AvatarChanged;
+
+    public void ChangeAvatar(object? sender, string? name)
+    {
+        this.AvatarName = name;
+        this.AvatarChanged?.Invoke(sender, new EventArgs());
+    }
+}
